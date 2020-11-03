da9d3dc0277bd8885a63632ddbad3a888c6d2919
Markus Heiss <markusheiss@Markuss-MacBook-Pro.local>
2020-11-03 16:25:55 +0100

    added html files

---
+++ projects/random_walker/sketch.js
@@ -0,0 +1,33 @@
+var x;
+var y;
+
+function setup() {
+  createCanvas(400, 400);
+  x = 200;
+  y = 200;
+  background(51);
+}
+
+function draw() {
+  stroke(255);
+  strokeWeight(2);
+  point(x, y);
+  print("x: " + x);
+  print("y: " + y);
+  var r = floor(random(4));
+  
+  switch(r) {
+    case 0:
+      x = x + 1;
+      break;
+    case 1:
+      x = x - 1;
+      break;
+    case 2:
+      y = y + 1;
+      break;
+    case 3:
+      y = y - 1;
+      break;
+   }
+}
